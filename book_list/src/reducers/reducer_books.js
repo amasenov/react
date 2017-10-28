38f88daa64e5f8ccb218dac8118cd5ba8dbe2587
amasenov <amasenov@outlook.com>
2017-10-28 20:04:29 +0200

    book list
    React and Redux app

---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'The Great Gatsby' },
+        { title: 'The Time Machine' },
+        { title: 'Lord of the Rings' },
+        { title: 'Game of Thrones' }
+    ]
+}
---
+++ book_list/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+ module.exports = {
+     entry: './src/index.js',
+     output: {
+         path: path.resolve(__dirname, 'bin'),
+         filename: 'app.bundle.js',
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
+     },
+     resolve: {
+       extensions: ["", ".js", ".jsx"]
+     },
+     devServer: {
+       contentBase: "./"
+     }
+ }
