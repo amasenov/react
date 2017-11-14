b66b3f3e85dc799db2d36a81405b905395ee7175
amasenov <amasenov@outlook.com>
2017-11-14 19:45:40 +0100

    v. 1.4
    Added Firebas Integration

--- burger-app/src/hoc/Layout/Layout.js
+++ burger-app/src/hoc/Layout/Layout.js
@@ -8,7 +8,7 @@ import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
     state = {
         showSideDrawer: false
-    }
+    };
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
